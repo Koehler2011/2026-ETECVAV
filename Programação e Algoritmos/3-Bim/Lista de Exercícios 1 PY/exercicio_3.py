69c3eb86918943f66b06a19b836edf823e1b5b3c
Eduardo Koehler <okdudu02@gmail.com>
2026-08-14 12:36:52 -0300

    Create exercicio_3.py

---
+++ Programação e Algoritmos/3-Bim/Lista de Exercícios 1 PY/exercicio_3.py
@@ -0,0 +1,13 @@
+n = int(input('Digite um número inteiro positivo: '))
+soma = 0
+contador = 0
+
+
+if n > 0:
+    while contador <= n:
+        if contador % 2 == 0:
+            soma += contador
+        contador += 1
+    print(f'{soma}')           
+else:
+    print('Seu número é negativo ou zero, digite um positivo')
