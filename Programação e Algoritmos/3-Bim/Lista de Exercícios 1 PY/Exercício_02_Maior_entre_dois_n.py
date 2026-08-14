b1252c00f0094629b760bfab32854a69ea877485
Eduardo Koehler <okdudu02@gmail.com>
2026-08-14 11:38:42 -0300

    Create Exercício_02_Maior_entre_dois_n.py

---
+++ Programação e Algoritmos/3-Bim/Lista de Exercícios 1 PY/Exercício_02_Maior_entre_dois_n.py
@@ -0,0 +1,9 @@
+n = int(input('Digite um número inteiro: '))
+n2 = int(input('Digite outro número inteiro: '))
+
+if n > n2:
+    print(f'O número {n} é maior que {n2}')
+elif n < n2:
+    print(f'O número {n} é menor que {n2}')
+else:
+    print('Os números são iguais')
