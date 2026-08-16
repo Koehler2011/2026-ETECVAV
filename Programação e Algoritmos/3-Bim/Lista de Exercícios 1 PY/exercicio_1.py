db017828eca716794363354b7f5eab2fe24b311e
Eduardo Koehler <okdudu02@gmail.com>
2026-08-16 08:49:59 -0300

    Update exercicio_1.py

--- Programação e Algoritmos/3-Bim/Lista de Exercícios 1 PY/exercicio_1.py
+++ Programação e Algoritmos/3-Bim/Lista de Exercícios 1 PY/exercicio_1.py
@@ -1,3 +1,9 @@
+/*
+    Autor: Eduardo koehler
+    Data: Agosto/2026
+    Descrição: Lê um número inteiro e informa se ele é positivo, negativo ou zero.
+*/
+
 n = int(input('Escolha um número inteiro: '))
 if n > 0:
     print(f'O número {n} é positivo')
