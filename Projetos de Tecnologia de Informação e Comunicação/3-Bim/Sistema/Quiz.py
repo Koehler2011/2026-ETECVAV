b8feb1ae7e145cca5eeaececeb6c200a1a934b00
Eduardo Koehler <okdudu02@gmail.com>
2026-08-23 20:07:13 -0300

    Update Quiz.py

--- Projetos de Tecnologia de Informação e Comunicação/3-Bim/Sistema/Quiz.py
+++ Projetos de Tecnologia de Informação e Comunicação/3-Bim/Sistema/Quiz.py
@@ -1,3 +1,8 @@
+## Versão Inicial do Quiz
+
+```python
+
+---
 """
     Quiz de cantores sertanejos, criado por Eduardo Koehler 1°D
 """
@@ -112,3 +117,5 @@ perguntas += 1
 
 pontuacao_final = (acertos * 100)/perguntas
 print(f'Ah, este quiz chegou ao seu fim...maaass, irei revelar os seus stats! Sua pontuação final foi de {pontuacao_final:.2f}%, tendo {acertos} acerto(s) e {erros} erro(s)')
+```
+---
