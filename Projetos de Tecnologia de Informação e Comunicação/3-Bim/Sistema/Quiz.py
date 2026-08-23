10e0fe2e78eea6dade886e359bc2fa4ed82f6705
Eduardo Koehler <okdudu02@gmail.com>
2026-08-23 20:03:46 -0300

    Update Quiz.py

--- Projetos de Tecnologia de Informação e Comunicação/3-Bim/Sistema/Quiz.py
+++ Projetos de Tecnologia de Informação e Comunicação/3-Bim/Sistema/Quiz.py
@@ -5,6 +5,7 @@
 acertos = 0
 erros = 0
 pontuacao_final = 0
+perguntas = 0
 
 resposta_usuario = input('Este é um quiz de cantores sertanejos, gostaria de participar?\n')
 
@@ -29,6 +30,8 @@ else:
     erros += 1
     print('\nVocê errou! Vamos para a próxima.\n')
 
+perguntas += 1
+
 print('Segunda pergunta, que cantor é conhecido como "Embaixador"?')
 print('1) Gusttavo Lima')
 print('2) Gustavo Mioto')
@@ -43,7 +46,9 @@ else:
     erros += 1
     print('\nVocê errou! Não desanime!\n')
 
-print('Terceira pergunta, é verdade que a música "Boate Azul" surgiu por causa que o letrista passou pelo o que está na música?')
+perguntas += 1
+
+print('Terceira pergunta, é verdade que a música "Boate Azul" surgiu porque que o letrista passou pelo o que está na música?')
 print('1) Verdadeiro')
 print('2) Falso')
 
@@ -54,6 +59,8 @@ if pergunta == 1:
 else:
     erros += 1
     print('\nAh, você errou. Mas tudo bem, não desista!\n')
+
+perguntas += 1
 
 print('Quarta pergunta, quem canta a música "Te Esperando"?')
 print('1) Henrique e Juliano')
@@ -69,6 +76,8 @@ else:
     erros += 1
     print('\nVocê errou!\n')
 
+perguntas += 1
+
 print('Quinta pergunta, qual dupla sertaneja canta a música "Tijoão"?')
 print('1) Maiara e Maraísa')
 print('2) Yasmin Sensação')
@@ -83,19 +92,23 @@ else:
     erros += 1
     print('\nAh, que pena, você errou!\n')
 
-print('Sexta pergunta, qual dupla sertaneja canta a música "Conveniência", "namorada Reserva" e "Morena de Goiânia"?')
+perguntas += 1
+
+print('Sexta pergunta, qual dupla sertaneja canta a música "Conveniência", "Namorada Reserva" e "Morena de Goiânia"?')
 print('1) Guilherme e Benuto')
 print('2) Victor e Leo')
 print('3) Hugo e Guilherme')
-print('4) ')
+print('4) Bruno e Marrone')
 
 pergunta = int(input('Qual das alternativas é a sua resposta? '))
 if pergunta == 3:
     acertos += 1
-    print('\nMuuuuuuuuuuito bemm, sabe muito!\n')
+    print('\nIncrível!\n')
 else:
     erros += 1
-    print('\nAh, que pena, você errou!\n')
+    print('\nAh, você errou!\n')
 
-pontuacao_final = (acertos * 100)/10
-print(f'Após tudo isso, sua pontução final foi de {pontuacao_final}%, tendo {acertos} acerto(s) e {erros} erro(s)')
+perguntas += 1
+
+pontuacao_final = (acertos * 100)/perguntas
+print(f'Ah, este quiz chegou ao seu fim...maaass, irei revelar os seus stats! Sua pontuação final foi de {pontuacao_final:.2f}%, tendo {acertos} acerto(s) e {erros} erro(s)')
